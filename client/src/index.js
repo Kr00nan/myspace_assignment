593ca6ebe17228fae32e4eac46aadf1008e02c79
Michael Kroon <kroonology@gmail.com>
2019-09-25 16:41:40 -0600

    import AuthProvider/ BrowserRouter into index.js

--- client/src/index.js
+++ client/src/index.js
@@ -1,8 +1,15 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
+import { BrowserRouter } from 'react-router-dom';
+import { AuthProvider } from './providers/AuthProvider';
+import 'semantic-ui-css/semantic.min.css';
 
 ReactDOM.render(
-  <App />,
+  <AuthProvider>
+    <BrowserRouter>
+      <App />
+    </BrowserRouter>
+  </AuthProvider>,
   document.getElementById('root')
 );
