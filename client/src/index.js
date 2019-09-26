1fd76e50d61cc4e748f12e35d5aa4c051da7f4b9
Michael Kroon <kroonology@gmail.com>
2019-09-26 00:10:12 -0600

    build FetchUser and ProtectedRoute

--- client/src/index.js
+++ client/src/index.js
@@ -3,7 +3,11 @@ import ReactDOM from 'react-dom';
 import App from './App';
 import { BrowserRouter } from 'react-router-dom';
 import { AuthProvider } from './providers/AuthProvider';
+import { initMiddleware } from 'devise-axios';
 import 'semantic-ui-css/semantic.min.css';
+
+// helps the app keep track of new tokens everytime a request is made
+initMiddleware();
 
 ReactDOM.render(
   <AuthProvider>
