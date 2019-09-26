1d5a2ed1a1db2faa8e22add2295e08209582d5ec
Michael Kroon <kroonology@gmail.com>
2019-09-26 00:38:57 -0600

    add person model, people controller, api namespace

---
+++ db/migrate/20190926063601_create_people.rb
@@ -0,0 +1,12 @@
+class CreatePeople < ActiveRecord::Migration[6.0]
+  def change
+    create_table :people do |t|
+      t.string :name
+      t.string :location
+      t.string :birthday
+      t.string :avatar
+
+      t.timestamps
+    end
+  end
+end
