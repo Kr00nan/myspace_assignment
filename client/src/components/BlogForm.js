d895a9542f5195fe07597ae2c1b8475a7c3e2f31
Michael Kroon <kroonology@gmail.com>
2019-09-26 23:05:45 -0600

    create BlogForm and useFormInput

---
+++ client/src/components/BlogForm.js
@@ -0,0 +1,43 @@
+import React from 'react';
+import axios from 'axios';
+import { Form } from 'semantic-ui-react';
+import {useFormInput} from './hooks/useFormInput';
+
+const BlogForm = ({addBlog, toggleForm}) => {
+  // using custom hooks...
+  const title = useFormInput('');
+  const body = useFormInput('');
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    axios.post('/api/blogs', { title: title.value , body: body.value })
+      .then(res => {
+        addBlog(res.data);
+        toggleForm();
+
+      })
+      .catch(err => console.log('hello from the BlogForm handleSubmit catch...'));
+  }
+  return (
+    <Form onSubmit={handleSubmit}>
+      <Form.Group widths='equal'>
+        <Form.Input
+          label='Title'
+          placeholder='Title'
+          name='title'
+          required
+          {...title}
+        />
+        <Form.Input
+          label='Title'
+          placeholder='Title'
+          name='title'
+          required
+          {...body}
+        />
+      </Form.Group>
+    </Form>
+  );
+};
+
+export default BlogForm;
